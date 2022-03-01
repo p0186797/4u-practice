5692672a5ca571d769cbff6d7718b0bdbc4f74cf
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-02-28 20:42:35 -0500

    Create wordy_test.py

---
+++ wordy_test.py
@@ -0,0 +1,20 @@
+from wordy import *
+
+print(vowel_counter(["plunge"]))
+
+print(vowel_counter(["adieu", "boring"]))
+
+print(vowel_counter(["cat", "scramble", "telephone", "hungry", "hippo"]))
+
+print(vowel_counter(["ate", "ontario", "utter", "unique", "bat", "on", "i"]))
+
+print(sorted(most_frequent_vowel(["plunge"])))
+
+print(sorted(most_frequent_vowel(["adieu", "boring"])))
+
+print(sorted(most_frequent_vowel(["cat", "scramble", "telephone", "hungry", "hippo"])))
+
+print(sorted(most_frequent_vowel(["ate", "ontario", "utter", "unique", "bat", "on", "i"])))
+
+      
+    
